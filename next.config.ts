34eb77d4e03cab7b28779f2009a981e5e7d0fbf1
Emman442 <ndemaemmanuel82@gmail.com>
2026-06-25 12:18:49 +0100

    Fixed the issue with privy and solana

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.externals.push({
+      '@solana/kit': 'commonjs @solana/kit',
+      '@solana-program/memo': 'commonjs @solana-program/memo',
+      '@solana-program/system': 'commonjs @solana-program/system',
+      '@solana-program/token': 'commonjs @solana-program/token',
+    });
+    return config;
+  },
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
